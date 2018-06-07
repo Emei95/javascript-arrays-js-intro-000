c28e1d1dccc09b6c351c071e5fb55a78dd08917f
Eric Mei <shizizerick@aol.com>
2018-06-07 22:00:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array = array.slice(0,3)
+  array = array.slice(3)
   return array
 }
